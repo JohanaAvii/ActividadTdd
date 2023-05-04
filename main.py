26118e2f9f89300f12108e0f83afeb2e34edd80a
LAUREANO-CASTRELLON-PEREZ <72671180+LAUREANO-CASTRELLON-PEREZ@users.noreply.github.com>
2023-05-04 17:04:50 -0500

    Fallo NPrimos 1

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from math import sqrt
+
+app = FastAPI()
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello FastAPI"}
+
+@app.get("/IsPrime/{num}")
+def is_prime(num: int):
+    if num < 2:
+        return {"is_prime": False}
+    for i in range(2, int(sqrt(num))+1):
+        if num % i == 0:
+            return {"is_prime": False}
+    return {"is_prime": True}
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ test.py
@@ -0,0 +1,4 @@
+from main import is_prime
+
+def test_is_prime():
+    assert is_prime(2) == {"is_prime": True}
