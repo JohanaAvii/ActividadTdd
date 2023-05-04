8fc82c01e0ac378c1abe965955bf2fb366eb8fae
LAUREANO-CASTRELLON-PEREZ <72671180+LAUREANO-CASTRELLON-PEREZ@users.noreply.github.com>
2023-05-04 17:44:52 -0500

    Fallo fibonacci 1

--- main.py
+++ main.py
@@ -16,8 +16,22 @@ def is_prime(num: int):
             return {"is_prime": False}
     return {"is_prime": True}
 
+@app.get("/fibonacci/{pos}")
+def fibonacci(pos: int):
+    if pos == 0:
+        return {"fibonacci": 0}
+    elif pos == 1:
+        return {"fibonacci": 1}
+    else:
+        a = 0
+        b = 1
+        for i in range(2, pos+1):
+            c = a + b
+            a = b
+            b = c
+        return {"fibonacci": b}
 
-#hdsfhhfhdfjk
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- test.py
+++ test.py
@@ -2,3 +2,7 @@ from main import is_prime
 
 def test_is_prime():
     assert is_prime(2) == {"is_prime": True}
+
+
+def test_fibonacci():
+    assert fibonacci(8) == {"fibonacci": 21}
