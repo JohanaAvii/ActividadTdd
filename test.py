5d686ce1fe49719cd10f70655bd2e7183cac9abd
LAUREANO-CASTRELLON-PEREZ <72671180+LAUREANO-CASTRELLON-PEREZ@users.noreply.github.com>
2023-05-04 17:48:30 -0500

    Fallo fibonacci

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-from main import is_prime
+from main import fibonacci, is_prime
 
 def test_is_prime():
     assert is_prime(2) == {"is_prime": True}
 
 
+
+#####
 def test_fibonacci():
-    assert fibonacci(8) == {"fibonacci": 21}
+    assert fibonacci(-1) == {"fibonacci": 0}
