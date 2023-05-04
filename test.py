594c9600dcf523a8dee1d43253855df26a6cae49
LAUREANO-CASTRELLON-PEREZ <72671180+LAUREANO-CASTRELLON-PEREZ@users.noreply.github.com>
2023-05-04 17:50:47 -0500

    Exito Fibonacci

--- test.py
+++ test.py
@@ -3,8 +3,5 @@ from main import fibonacci, is_prime
 def test_is_prime():
     assert is_prime(2) == {"is_prime": True}
 
-
-
-#####
 def test_fibonacci():
-    assert fibonacci(-1) == {"fibonacci": 0}
+    assert fibonacci(8) == {"fibonacci": 21}
